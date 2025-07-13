3b03e0c79f68e8c78d3fda708c42bf6b25fd5ab1
Rossignol Alex <alexnw33910@gmail.com>
2025-07-13 23:44:44 +0200

    fix: import component in page and fix metadata in layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
-import WordsRotate from "@/components/elements/words-rotate";
-import "./page.css";
+"use client";
+
+import Hero from "@/components/layouts/hero";
+import Header from "@/components/blocks/header";
+import Project from "@/components/layouts/project";
+
+import "@/styles/globals.css";
+import Skill from "@/components/layouts/skill";
 
 export default function Home() {
   return (
     <>
-      <div className="home_container">
-        <div className="home_text">
-          <h2>Developer of clear, reliable web and</h2>
-          <h2> mobile solutions. Clean code,</h2>
-          <h2>
-            crafted for <WordsRotate words={["you.", "us.", "all."]} />
-          </h2>
-        </div>
-        <aside>hello</aside>
-      </div>
+      <Header />
+      <main>
+        <Hero />
+        <Project />
+        <Skill />
+      </main>
     </>
   );
 }

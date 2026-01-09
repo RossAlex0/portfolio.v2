85df67b482914b55ca1662fbead60fbb49d26474
Rossignol Alex <alexnw33910@gmail.com>
2026-01-09 22:30:34 +0100

    feat: create social link component

--- src/components/ui/social-link/index.tsx
+++ src/components/ui/social-link/index.tsx
@@ -3,28 +3,41 @@
 import { motion } from "framer-motion";
 import CustomIcon from "@/components/ui/custom-icon";
 import { SocialLinkProps } from "./type";
+import Link from "next/link";
 
 export default function SocialLink({
   href,
   iconName,
-  delay,
   label,
+  delay = 0,
   size = 36,
+  light,
+  disabledAnimation,
   target = "_blank",
 }: SocialLinkProps) {
+  const animation = disabledAnimation
+    ? {}
+    : {
+        initial: { opacity: 0, x: -25 * (delay + 1) },
+        whileInView: { opacity: 1, x: 0 },
+        transition: { duration: 0.2 * (delay + 1) },
+        viewport: { amount: 1, once: true },
+      };
+
   return (
-    <motion.a
+    <Link
       href={href}
       target={target}
       rel="noopener noreferrer"
       aria-label={label}
-      whileHover={{ scale: 1.2 }}
-      initial={{ opacity: 0, x: -25 * (delay + 1) }}
-      whileInView={{ opacity: 1, x: 0 }}
-      transition={{ duration: 0.2 * (delay + 1) }}
-      viewport={{ amount: 1, once: true }}
     >
-      <CustomIcon name={iconName} size={size} />
-    </motion.a>
+      <motion.div whileHover={{ scale: 1.2 }} {...animation}>
+        <CustomIcon
+          name={iconName}
+          size={size}
+          color={light ? "#fffcee" : "#171717"}
+        />
+      </motion.div>
+    </Link>
   );
 }

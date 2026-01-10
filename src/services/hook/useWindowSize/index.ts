8a9831ae1b9238f7dafa2bb9ca6bf8d5ef3ec70e
Rossignol Alex <alexnw33910@gmail.com>
2026-01-10 23:02:11 +0100

    feat: create use window size hook

---
+++ src/services/hook/useWindowSize/index.ts
@@ -0,0 +1,31 @@
+"use client";
+
+import React from "react";
+
+type WindowSize = {
+  width?: number;
+  height?: number;
+};
+
+export function useWindowSize(): WindowSize {
+  const [windowSize, setWindowSize] = React.useState<WindowSize>({
+    width: undefined,
+    height: undefined,
+  });
+
+  React.useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowSize;
+}
